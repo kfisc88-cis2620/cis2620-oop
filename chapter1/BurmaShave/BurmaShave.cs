fd9e48523abc9f34bb424a57b3cb67dafe383540
arkais <krfisc@outlook.com>
2020-01-19 12:13:42 -0600

    Finished Exercise 9

---
+++ chapter1/BurmaShave/BurmaShave.cs
@@ -0,0 +1,21 @@
+﻿// Filename: BurmaShave
+// Written by: Kelley Fischer
+// Written on: 01/19/20
+
+using static System.Console;
+
+namespace BurmaShave
+{
+    class BurmaShave
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("It's Best For");
+            WriteLine("One Who Hits");
+            WriteLine("The Bottle");
+            WriteLine("To Let Another");
+            WriteLine("Use The Throttle");
+            WriteLine("Burma Shave");
+        }
+    }
+}
