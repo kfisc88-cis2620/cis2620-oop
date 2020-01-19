7dc75accc71d3f1ae3386ff1d491867c5b8c8a09
arkais <krfisc@outlook.com>
2020-01-19 12:04:20 -0600

    Initial commit

---
+++ chapter1/ch1_ex1/Hello.cs
@@ -0,0 +1,18 @@
+﻿// Filename: Hello.cs
+// Written by: Kelley Fischer
+// Written on: 01/19/20
+
+using static System.Console;
+
+namespace ch1_ex1
+{
+    class Program
+    {
+        static void Main()
+            /* This program demonstrates the use of the WriteLine()
+             * to display the message, "Hello, World!" */
+        {
+            WriteLine("Hello, world!");
+        }
+    }
+}
