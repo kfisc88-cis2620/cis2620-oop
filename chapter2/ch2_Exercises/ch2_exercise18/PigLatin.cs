57d61db0112385576a608afc9fcfc76ef4044425
arkais <krfisc@outlook.com>
2020-01-24 20:33:02 -0600

    finished exercise for chapter 2 exercise 2

---
+++ chapter2/ch2_Exercises/ch2_exercise18/PigLatin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+// Filename: PigLatin.cs
+// Written by: Kelley Fischer
+// Written on: 01/23/20
+// Description: 
+//      Chapter 2 - Exercise 18
+//      Converts a user input word into the pseduo-langauge Pig Latin
+// Encountered Issues: None;
+
+namespace ch2_exercise18
+{
+    class PigLatin
+    {
+        static void Main(string[] args)
+        {
+            string word, pigLatinWord;
+
+            Console.WriteLine("Enter a word: ");
+            word = Console.ReadLine();
+
+            // Converts the user input word into Pig Latin
+            pigLatinWord = word.Substring(1, word.Length-1) + word.Substring(0, 1) + "ay";
+
+            Console.WriteLine("The Pig Latin translation of '{0}' is '{1}'", word, pigLatinWord);
+        }
+    }
+}
--- chapter2/ch2_Exercises/ch2_exercise4/DoubleDecimalTest.cs
+++ chapter2/ch2_Exercises/ch2_exercise4/DoubleDecimalTest.cs
@@ -3,7 +3,7 @@
 // Written on: 01/22/20
 // Description: 
 //      Chapter 2 - Exercise 4
-//      
+//      This program doesn't really do anything, it was just to test the limits of decimal and double.
 // Encountered Issues: None;
 
 using System;
@@ -14,8 +14,8 @@ namespace ch2_exercise4
     {
         static void Main()
         {
-            double value1 = 123456789101112131415161718192.00;
-            decimal value2 = 123456789101112131415161718192.00M;
+            double value1 = 123456789101112131415161718192.0;
+            decimal value2 = 123456789101112131415161718192.0M;
             Console.WriteLine($"{value1} and {value2} are numbers!!");
         }
     }
