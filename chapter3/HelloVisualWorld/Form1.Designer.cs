767ff98dd2c9b4a4f7f0432e6c79d47fce7e01dd
arkais <krfisc@outlook.com>
2020-01-28 23:33:13 -0600

    added ch3 folder, completed YDI exercise

---
+++ chapter3/HelloVisualWorld/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace HelloVisualWorld
+{
+    partial class HelloForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayOutputButton = new System.Windows.Forms.Button();
+            this.helloLabel = new System.Windows.Forms.Label();
+            this.changeOutputButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayOutputButton
+            // 
+            this.displayOutputButton.BackColor = System.Drawing.Color.Aqua;
+            this.displayOutputButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.displayOutputButton.Location = new System.Drawing.Point(80, 50);
+            this.displayOutputButton.Name = "displayOutputButton";
+            this.displayOutputButton.Size = new System.Drawing.Size(75, 23);
+            this.displayOutputButton.TabIndex = 0;
+            this.displayOutputButton.Text = "Click here";
+            this.displayOutputButton.UseVisualStyleBackColor = false;
+            this.displayOutputButton.Click += new System.EventHandler(this.DisplayOutputButton_Click);
+            // 
+            // helloLabel
+            // 
+            this.helloLabel.AutoSize = true;
+            this.helloLabel.Font = new System.Drawing.Font("Magneto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helloLabel.Location = new System.Drawing.Point(80, 140);
+            this.helloLabel.Name = "helloLabel";
+            this.helloLabel.Size = new System.Drawing.Size(183, 20);
+            this.helloLabel.TabIndex = 1;
+            this.helloLabel.Text = "Hello, Visual World!";
+            this.helloLabel.Visible = false;
+            // 
+            // changeOutputButton
+            // 
+            this.changeOutputButton.Enabled = false;
+            this.changeOutputButton.Location = new System.Drawing.Point(80, 195);
+            this.changeOutputButton.Name = "changeOutputButton";
+            this.changeOutputButton.Size = new System.Drawing.Size(75, 23);
+            this.changeOutputButton.TabIndex = 2;
+            this.changeOutputButton.Text = "Click me last";
+            this.changeOutputButton.UseVisualStyleBackColor = true;
+            this.changeOutputButton.Click += new System.EventHandler(this.ChangeOutputButton_Click);
+            // 
+            // HelloForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.changeOutputButton);
+            this.Controls.Add(this.helloLabel);
+            this.Controls.Add(this.displayOutputButton);
+            this.Name = "HelloForm";
+            this.Text = "Hello Visual World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayOutputButton;
+        private System.Windows.Forms.Label helloLabel;
+        private System.Windows.Forms.Button changeOutputButton;
+    }
+}
+
